c4ef2419e717d51484e34007c12d15f831d871b6
Ross <rgoncharuk@gmail.com>
2016-03-05 16:17:39 +0200

    Initial commit

---
+++ src/com/brainacad/oop/testsregexp/Main.java
@@ -0,0 +1,28 @@
+package com.brainacad.oop.testsregexp;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] arr = {"VOVA","Ivan","R2d2","ZX","Anna","12345","ToAd","TomCat"," "};
+
+        for(String arrayElement : arr){
+            if(checkPersonWithRegExp(arrayElement) == true){
+                System.out.println(arrayElement);
+            }
+        }
+    }
+    public static boolean checkPersonWithRegExp(String userNameString){
+
+        String upperCase = "[A-Z]+\\D[a-z]+";
+
+        Pattern pattern = Pattern.compile(upperCase);
+        Matcher matcher = pattern.matcher(userNameString);
+        boolean matches = matcher.matches();
+
+        return matches;
+    }
+}
